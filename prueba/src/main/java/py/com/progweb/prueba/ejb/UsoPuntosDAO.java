f5e8962e3f48d29fd3fad20b5656070c8dd78547
everfgmolinasFPUNA <everfgmolinas@fpuna.edu.py>
2022-04-04 08:46:55 -0400

    Servicio de uso de puntos

--- prueba/src/main/java/py/com/progweb/prueba/ejb/UsoPuntosDAO.java
+++ prueba/src/main/java/py/com/progweb/prueba/ejb/UsoPuntosDAO.java
@@ -7,6 +7,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.sql.Date;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +25,20 @@ public class UsoPuntosDAO {
 
         puntosDAO = new ConceptoUsoPuntosDAO();
         bolsaPuntosDAO = new BolsaPuntosDAO();
-        Query qConcep = this.em.createQuery("select c from ConceptoUsoPuntos c WHERE c.idConceptoUsoPuntos = ?1");
-        qConcep.setParameter(1, entidad.getIdConcepto());
-        ConceptoUsoPuntos concepto = (ConceptoUsoPuntos) qConcep.getResultList();
-
+        ConceptoUsoPuntos concepto = this.em.find(ConceptoUsoPuntos.class, entidad.getIdConcepto());
+        if(concepto == null)
+            throw new QueryException("No existe un concepto registrado con ese id");
         // Cantidad de puntos a utilizar
         int puntosRequeridos = concepto.getPuntosRequeridos();
         int puntosUtilizados = 0;
 
+        // Busqueda de cliente
+        Cliente cliente = this.em.find(Cliente.class, entidad.getIdCliente());
+        if(cliente == null)
+            throw new QueryException("No existe el cliente");
+
         // Bolsas disponibles para uso
-        Query q = this.em.createQuery("select b from BolsaPuntos b where b.id_cliente = ?1 and b.saldo_puntos > ?2 order by b.fecha_asignacion asc" );
+        Query q = this.em.createQuery("select b from BolsaPuntos b where b.id_cliente = ?1 and b.saldo_puntos > ?2 order by b.fecha_asignacion asc, b.id_bolsa_puntos asc " );
         q.setParameter(1, entidad.getIdCliente());
         q.setParameter(2, 0);
         List<BolsaPuntos> bolsas = (List<BolsaPuntos>)q.getResultList();
@@ -45,7 +50,7 @@ public class UsoPuntosDAO {
         for(BolsaPuntos b : bolsas){
             bolsasUtilizadas.add(b);
             puntosUtilizados+= b.getSaldo_puntos();
-            if(puntosUtilizados + b.getSaldo_puntos() >= puntosRequeridos){
+            if(puntosUtilizados >= puntosRequeridos){
                 break;
             }
 
@@ -59,6 +64,8 @@ public class UsoPuntosDAO {
         Cabecera cabecera = new Cabecera();
         cabecera.setIdCliente(entidad.getIdCliente());
         cabecera.setPuntajeUtilizado(puntosRequeridos);
+        cabecera.setIdConceptoPuntos(entidad.getIdConcepto());
+        cabecera.setFechaUso(Date.valueOf(java.time.LocalDate.now()));
         this.em.persist(cabecera);
 
         // Crear detalle
@@ -66,21 +73,31 @@ public class UsoPuntosDAO {
         detalle.setIdCabecera(cabecera.getIdCabecera());
         detalle.setPuntajeUtilizado(puntosRequeridos);
         this.em.persist(detalle);
-
+        puntosUtilizados = 0;
         // Consumir puntos de las bolsas
         for(BolsaPuntos b : bolsasUtilizadas){
             if(b.getSaldo_puntos() - puntosRequeridos < 0){
-                this.bolsaPuntosDAO.updatePuntos(b.getId_bolsa_puntos(), b.getSaldo_puntos());
+                BolsaPuntos e = this.em.find(BolsaPuntos.class, b.getId_bolsa_puntos());
+                puntosUtilizados = e.getSaldo_puntos();
+                e.setPuntos_utilizados(e.getPuntos_utilizados() + b.getSaldo_puntos());
+                e.setSaldo_puntos(e.getPuntos_totales() - e.getPuntos_utilizados());
+
+                this.em.merge(e);
             }
             else {
-                this.bolsaPuntosDAO.updatePuntos(b.getId_bolsa_puntos(), puntosRequeridos);
+                BolsaPuntos e = this.em.find(BolsaPuntos.class, b.getId_bolsa_puntos());
+                puntosUtilizados = e.getSaldo_puntos();
+                e.setPuntos_utilizados(e.getPuntos_utilizados() + puntosRequeridos);
+                e.setSaldo_puntos(e.getPuntos_totales() - e.getPuntos_utilizados());
+
+                this.em.merge(e);
             }
             // Enlazar detalle con bolsas utilizadas
             DetalleBolsaPuntosManytoMany m2m = new DetalleBolsaPuntosManytoMany();
             m2m.setIdDetalle(detalle.getIdDetalle());
             m2m.setIdBolsaPuntos(b.getId_bolsa_puntos());
             this.em.persist(m2m);
-            puntosRequeridos-= b.getSaldo_puntos();
+            puntosRequeridos-= puntosUtilizados;
         }
 
     }
